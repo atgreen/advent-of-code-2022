3724ad1e75fc89d52a4481d96fe6a0c3c8a0b277
Anthony Green <green@moxielogic.com>
2022-12-01 00:27:55 -0500

    Hello world

---
+++ 01.lisp
@@ -0,0 +1,7 @@
+(ql:quickload :split-sequence)
+
+(let ((cals (sort (mapcar (lambda (nums) (reduce #'+ (mapcar #'parse-integer nums)))
+                          (split-sequence:split-sequence "" (uiop:read-file-lines "01.input") :test #'equal))
+                  #'>)))
+  (print (apply #'max cals))
+  (print (+ (car cals) (cadr cals) (caddr cals))))
