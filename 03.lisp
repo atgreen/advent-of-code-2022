01356c409358fbad068ba9a5d20e3f1c9c132a77
Anthony Green <green@moxielogic.com>
2022-12-03 08:19:21 -0500

    No need for fset

--- 03.lisp
+++ 03.lisp
@@ -1,17 +1,14 @@
-(ql:quickload :fset)
-
 (let ((input (uiop:read-file-lines "03.input")))
 
   (print (loop for line in input
                sum (let* ((l (length line))
-                          (c (char-int (fset:arb (fset:intersection
-                                                  (fset:convert 'fset:set (subseq line 0 (/ l 2)))
-                                                  (fset:convert 'fset:set (subseq line (/ l 2))))))))
-                     (if (<= 65 c 90) (- c 38) (- c 96)))))
+                          (c (char-int (car (intersection (coerce (subseq line 0 (/ l 2)) 'list)
+                                                          (coerce (subseq line (/ l 2)) 'list))))))
+                     (if (<= c 90) (- c 38) (- c 96)))))
 
   (print (loop while input
-               sum (let ((c (char-int (fset:arb (reduce #'fset:intersection
-                                                          (list (fset:convert 'fset:set (pop input))
-                                                                (fset:convert 'fset:set (pop input))
-                                                                (fset:convert 'fset:set (pop input))))))))
-                     (if (<= 65 c 90) (- c 38) (- c 96))))))
+               sum (let ((c (char-int (car (reduce #'intersection
+                                                     (list (coerce (pop input) 'list)
+                                                           (coerce (pop input) 'list)
+                                                           (coerce (pop input) 'list)))))))
+                     (if (<= c 90) (- c 38) (- c 96))))))
